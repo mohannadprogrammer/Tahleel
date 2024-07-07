11cc9753e593be09b72db644705dcdd773b09983
mohannad waheed <waheedmohannad55578@gmail.com>
2024-07-06 17:26:00 -0700

    create contact page

---
+++ contact.php
@@ -0,0 +1,14 @@
+
+    <?php
+    $title = 'Add Hospetl';
+    require_once './template/header.php';
+    // require_once './functions/database_functions.php';
+    ?>
+    <main>
+    <article>
+    <section class="about" id="about"></section>
+    </artical>
+    </main>
+    <?php
+    require_once './template/footer.php';
+    ?>
