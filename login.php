a3630bfd0e662862da834be65079d0f9f78535c2
mohannad waheed <waheedmohannad55578@gmail.com>
2024-07-11 09:40:01 -0700

    fix header redirict code

--- login.php
+++ login.php
@@ -4,17 +4,24 @@
 	require "./template/header.php";
 	require_once "./functions/database_functions.php";
 
-
+    ob_start();
 	if(isset( $_POST['email']) && isset($_POST['pass'])) {
         $row = login($_POST['email'] ,$_POST['pass']);
         // $_SESSION['user_info'] = '';
 	    $_SESSION['user_info'] =$row;
+        // echo $row['email_user'];
+        // echo "<script> window.location.href = './index.php'</script>";
+
 			
-	if(!$row){
-		echo '<p class="lead text-center text-muted">عفوا هذا المتخدم غير موجود </p>';
-	}
+	if(!isset($row['email_user'])){
+		echo '<p class="callout">عفوا هذا المتخدم غير موجود </p>';
+	}else{
+        echo "<script> window.location.href = './index.php'</script>";
+
+
+    }
     // redirct("index.php");
-	header("Location: ./index.php");
+	// header("Location: index.php");
     $massage="";
 }
 
@@ -27,7 +34,6 @@
             <form autocomplete="off" method="post" class  = "form"enctype="application/x-www-form-urlencoded">
            
         <h1>login</h1>
-           
 			<br>
             <div class="input">
                 <input required type="text" name="email" id="ucname" maxlength="50" placeholder="name">
