0cedfc8dbac3748992426087e4ef734263131da6
mohannad waheed <waheedmohannad55578@gmail.com>
2024-07-13 09:22:22 -0700

    modification blogs page

---
+++ admin_blogs.php
@@ -0,0 +1,74 @@
+
+<?php
+    $title = 'Add Hospetl';
+    require_once './template/header.php';
+    require_once './functions/database_functions.php';
+    $name="";
+    $email="";
+    $phone="";
+    $massage="";
+
+    $result1 = getAllBlog();
+
+
+
+    ?>
+    <main>
+    <article>
+    <section class="contact" >
+    <div class="form-container" >
+    <div class = 'menu'>
+             
+             <a href="./profile.php" >profile</a>
+             <a href="./masseges.php" >Massages</a>
+             <a href="./properties.php" >Properties</a>
+             <a href="./admin_blogs.php" class="active">Blogs</a>
+         </div>
+        <div class = 'table'>
+        <table class="data">
+        <thead>
+            <tr>
+            <th>Subject</th> 
+            <th>topic</th> 
+            <th>content</th> 
+            <th>created date</th> 
+            <th>created by</th> 
+            <th>topic image</th> 
+            </tr>
+        </thead>
+        <tbody>
+        <?php 
+                    if($result1){
+                        foreach ($result1 as $row){
+?>
+            <tr>
+                
+                <td><?=$row['subject']?></td>
+                <td><?=$row['topic_name']?></td>
+                <td><?=$row['content']?></td>
+                <td><?=$row['created_date']?></td>
+                <td><?=$row['user_name']?></td>
+                <td><?=$row['topic_image']?></td>
+            </tr>
+
+<?php
+                    }}else{
+?>
+
+<?php
+                    }
+                    ?>
+                
+
+           
+        </tbody>
+            </table>
+        </div>
+        
+        </div>
+    </section>
+    </artical>
+    </main>
+    <?php
+    require_once './template/footer.php';
+    ?>
